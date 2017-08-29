a9e5538ce9c67cb89b9b5c5ae846a3a8e65b82f3
Juuso Pakarinen <juusojpak@gmail.com>
2017-08-30 02:20:40 +0300

    Add first styling.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
   'use strict';
 
   function scrollToDiv(destination, duration, callback) {
-    const start = window.pageYOffset;
+    const startPosition = window.pageYOffset;
     const startTime = new Date().getTime();
 
     const documentHeight = Math.max(
@@ -31,14 +31,13 @@
     if ('requestAnimationFrame' in window === false) {
       window.scrollTo(0, destinationOffset);
       if (callback) callback();
-
       return null;
     }
 
     function scroll() {
       const now = new Date().getTime();
       const time = Math.min(1, ((now - startTime) / duration));
-      window.scroll(0, Math.ceil((time * (destinationOffset - start)) + start));
+      window.scroll(0, Math.ceil((time * (destinationOffset - startPosition)) + startPosition));
 
       if (window.pageYOffset === destinationOffset) {
         if (callback) callback();
@@ -49,6 +48,31 @@
     }
 
     scroll();
+  }
+
+  function checkSkillImagesInView() {
+    const windowTop = window.pageYOffset;
+    const windowBottom = windowTop + window.innerHeight;
+    const elementTop = skillsContainer.offsetTop;
+    const elementBottom = elementTop + skillsContainer.offsetHeight;
+
+    if (elementTop <= windowBottom && elementBottom >= windowTop ) {
+      for (var i = 0; i < skillElements.length; i++) {
+        var rnd = Math.floor(Math.random() * 3);
+
+        switch (rnd) {
+          case 1:
+            skillElements[i].className += " bounceInSlow";
+            break;
+          case 2:
+            skillElements[i].className += " bounceInFast";
+            break;
+          default:
+            skillElements[i].className += " bounceInNormal";
+            break;
+        }
+      }
+    }
   }
 
   var links = document.getElementsByClassName("headerLink");
@@ -65,5 +89,10 @@
   links[3].addEventListener("click", function () {
     scrollToDiv(document.getElementById("about"), 280, null);
   });
+
+  var skillsContainer = document.getElementById("skillContainer");
+  var skillElements = document.getElementsByClassName("skill");
+  window.addEventListener("scroll", checkSkillImagesInView);
+  window.addEventListener("resize", checkSkillImagesInView);
 
 })();
